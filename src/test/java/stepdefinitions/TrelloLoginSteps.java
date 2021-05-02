6164d1b42a721510a9d9fced3aaa6dc25f5cb0d2
Michel Nicholas <michelnicholas1993@gmail.com>
2021-05-02 13:52:42 -0400

    Implemented two new methods for TrellLoginSteps

--- src/test/java/stepdefinitions/TrelloLoginSteps.java
+++ src/test/java/stepdefinitions/TrelloLoginSteps.java
@@ -3,19 +3,21 @@ package stepdefinitions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import com.github.javafaker.Faker;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class TrelloLoginSteps {
-    
-    
-    private void pause(int second){
-        try{
+
+    private void pause(int second) {
+        try {
             Thread.sleep(second * 1000);
 
-        }catch(InterruptedException ignore){
-            // do nothing 
+        } catch (InterruptedException ignore) {
+            // do nothing
         }
     }
 
@@ -38,18 +40,19 @@ public class TrelloLoginSteps {
         WebElement pageBannerElem = Hooks.driver().findElement(loc_page_banner);
         boolean isVisible = pageBannerElem.isDisplayed();
         Assert.assertTrue(isVisible);
-}
+    }
 
     @When("^User logins to the trello with valid credentials$")
-    public void user_logins_to_the_trello_with_valid_credentials()  {
+    public void user_logins_to_the_trello_with_valid_credentials() {
         By loc_email_field = By.id("user");
         By loc_password_field = By.id("password");
         By loc_login_button = By.id("login");
         WebElement emailField = Hooks.driver().findElement(loc_email_field);
         WebElement passwordField = Hooks.driver().findElement(loc_password_field);
+        emailField.sendKeys("michelnicholas1993@gmail.com");
+        passwordField.sendKeys("Imani11234");
         WebElement loginButton = Hooks.driver().findElement(loc_login_button);
         loginButton.click();
-
     }
 
     @Then("^User should be able to see Trello boards page$")
@@ -59,7 +62,31 @@ public class TrelloLoginSteps {
         WebElement pageBanner = Hooks.driver().findElement(loc_page_banner);
         boolean isVisible = pageBanner.isDisplayed();
         Assert.assertTrue(isVisible);
-        }
+    }
 
+    @When("^user logins with invalid credentials$")
+    public void user_logins_with_invalid_credentials() {
+        By loc_email_field = By.id("user");
+        By loc_password_field = By.id("password");
+        By loc_login_button = By.id("login");
+        WebElement emailField = Hooks.driver().findElement(loc_email_field);
+        WebElement passwordField = Hooks.driver().findElement(loc_password_field);
+        Faker faker = new Faker();
+        String fakeEmail = faker.internet().emailAddress();
+        String fakePassword = faker.internet().password();
+        emailField.sendKeys(fakeEmail);
+        passwordField.sendKeys(fakePassword);
+        WebElement loginButton = Hooks.driver().findElement(loc_login_button);
+        loginButton.click();
+    }
+
+    @Then("^user should be able to see an error message$")
+    public void user_should_be_able_to_see_an_error_message() {
+        pause(3);
+        By loc_error_msg = By.xpath("//div[@id='error']/p");
+        WebElement errorMsgElement = Hooks.driver().findElement(loc_error_msg);
+        String actualErrorMsg = errorMsgElement.getText();
+        Assert.assertEquals("There isn't an account for this email",actualErrorMsg);
+    }
 
 }
